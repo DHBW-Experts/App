9f28e32cc89e67f31f949e388ec7e1036769f386
Ralph-1006 <ralphboehm2002@yahoo.de>
2022-04-09 22:26:23 +0200

    add logic to foreign profile to display validation

--- src/app/tabs/view-foreign-profile/view-foreign-profile.page.ts
+++ src/app/tabs/view-foreign-profile/view-foreign-profile.page.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-foreign-profile.page.scss'],
 })
 export class ViewForeignProfilePage implements OnInit {
+  user: User;
+  tags: Tag[];
+  tagValidations = [];
+  isDataAvailable: boolean = false;
+  isTagSelected: boolean = false;
+  selectedTag: Tag;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {}
 
-  constructor() { }
+  ionViewWillEnter() {
+    const persistence = new Persistence();
+    const userId = +this.route.snapshot.paramMap.get('id');
+    const userPromise = persistence.getUserById(userId);
+    userPromise.then((result) => {
+      this.user = result;
+      this.isDataAvailable = true;
 
   ngOnInit() {
   }
+    });
 
+    const tagPromise = persistence.getTags(userId);
+    tagPromise.then((result) => {
+      this.tags = result;
+    });
+  }
+
+  tagSelected(tag: Tag) {
+    this.isTagSelected = true;
+    this.selectedTag = tag;
+    let persistence = new Persistence();
+    const tagValidationsPromise = persistence.getTagValidation(tag.tagId);
+    tagValidationsPromise.then((result) => {
+      this.tagValidations = result.map((validation) => validation.comment);
+    });
+  }
 }
