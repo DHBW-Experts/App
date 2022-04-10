cc961624c6a87a1265e25ef732c378fdcd468d45
Ralph-1006 <ralphboehm2002@yahoo.de>
2022-04-10 21:55:15 +0200

    update dependencies

--- src/app/tabs/view-foreign-profile/view-foreign-profile.page.ts
+++ src/app/tabs/view-foreign-profile/view-foreign-profile.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { alertController } from '@ionic/core';
+import { LoginPage } from 'src/app/auth/login/login.page';
+import { Persistence } from 'src/app/models/Persistence';
+import { Tag } from 'src/app/models/tag';
+import { TagValidation } from 'src/app/models/tag-validation';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-view-foreign-profile',
