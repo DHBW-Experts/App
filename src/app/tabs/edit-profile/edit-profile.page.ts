9dd307fb4214187c22edb6acf6297cb6eabd6476
Tim Czerkas <t.handy@gmx.de>
2022-04-12 10:50:15 +0200

    DHEXP-98 PW-Doublecheck and Button Fix

--- src/app/tabs/edit-profile/edit-profile.page.ts
+++ src/app/tabs/edit-profile/edit-profile.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, AnimationBuilder, ToastController } from '@ionic/angular';
 import { LoginPage } from '../../auth/login/login.page';
 import { Persistence } from '../../models/persistence';
 
@@ -14,7 +15,7 @@ export class EditProfilePage implements OnInit {
   isDataAvailable: boolean = false;
   user: User = null;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private alertCtrl: AlertController, public toastController: ToastController) {
     const persistence = new Persistence();
     const userPromise = persistence.getUserById(LoginPage.user.userId);
     userPromise.then((result) => {
@@ -28,13 +29,33 @@ export class EditProfilePage implements OnInit {
   password: String;
   password_wdh: String;
   password_old: String;
-  edit() {
-    //change password has to be checked
+  async edit() {
+    if (this.password !== this.password_wdh) {
+      let alert = await this.alertCtrl.create({
+        header: 'Fehler',
+        message: 'Die Passwörter stimmen nicht überein!',
+        buttons: ['Ok'],
+      });
+      alert.present();
+      return;
+    }
     this.persistence.editUser(this.user);
     LoginPage.user = this.user;
+    this.backToProfilePage();
+    this.presentChanged();
   }
 
   backToProfilePage() {
     this.router.navigate(['tabs/profile']);
+  }
+
+  async presentChanged() {
+    const toast = await this.toastController.create({
+      message: '<ion-icon name="checkmark-outline"></ion-icon>  Deine Änderungen wurden gespeichert.',
+      position: 'top',
+      color: 'success',
+      duration: 800
+    });
+    toast.present();
   }
 }
