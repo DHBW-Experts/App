62464331bb7b33947d4eea62036fcad8db590305
Ralph-1006 <ralphboehm2002@yahoo.de>
2022-04-13 19:53:58 +0200

    insert logic to scan student id

--- src/app/tabs/scan/scan.page.ts
+++ src/app/tabs/scan/scan.page.ts
@@ -1,21 +1,47 @@
 import { Component } from '@angular/core';
-import { NFC, NfcTag } from '@ionic-native/nfc/ngx'
+import { Router } from '@angular/router';
+import { NFC, NfcTag } from '@ionic-native/nfc/ngx';
+import { alertController } from '@ionic/core';
 import { Subscription } from 'rxjs';
+import { Persistence } from 'src/app/models/Persistence';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-scan',
   templateUrl: 'scan.page.html',
-  styleUrls: ['scan.page.scss']
+  styleUrls: ['scan.page.scss'],
 })
 export class ScanPage {
   private nfcSub: Subscription;
 
-  constructor(private nfc: NFC) {}
+  constructor(private nfc: NFC, private route: Router) {}
 
   ionViewDidEnter() {
     // Subscribe NFC reader
     const flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V;
-    this.nfcSub = this.nfc.readerMode(flags).subscribe(this.nfcTagHandler, this.nfcErrHandler);
+    this.nfcSub = this.nfc.readerMode(flags).subscribe((tag) => {
+      // Prettify tag id
+      const tagId = tag.id
+        .map((i) => Math.abs(i).toString(16).toUpperCase().padStart(2, '0'))
+        .join(':');
+
+      console.log(`NFC: ${tagId}`);
+      const persistence = new Persistence();
+      const userPromise = persistence.getUserByRFID(tagId);
+
+      userPromise.then(async (result) => {
+        if (!result.userId) {
+          const alert = await alertController.create({
+            header: 'Fehler',
+            message: 'Nutzer nicht gefunden',
+            buttons: ['Ok'],
+          });
+          await alert.present();
+          return;
+        }
+        this.route.navigate(['../view-foreign-profile', { id: result.userId }]);
+      });
+    }, this.nfcErrHandler);
   }
 
   ionViewDidLeave() {
@@ -25,16 +51,20 @@ export class ScanPage {
 
   nfcTagHandler(tag: NfcTag) {
     // Prettify tag id
-    const tagId = tag.id.map(i => Math.abs(i).toString(16).toUpperCase().padStart(2, '0')).join(':');
+    const tagId = tag.id
+      .map((i) => Math.abs(i).toString(16).toUpperCase().padStart(2, '0'))
+      .join(':');
 
     console.log(`NFC: ${tagId}`);
-
-    // TODO: redirect to profile page
+    const persistence = new Persistence();
+    const userPromise = persistence.getUserByRFID('TEST-RFID-ID-0815'); //tagID
+    userPromise.then((result) => {
+      this.route.navigate(['../view-foreign-profile', { id: result.userId }]); //Problem: cant acces router due to "this". solution: arrow func. (see nfc.readerMode(flags).subscribe)
+    });
   }
 
   nfcErrHandler(err: any) {
     console.log('Error reading tag', err);
-
     // TODO: show error in UI
   }
 }
