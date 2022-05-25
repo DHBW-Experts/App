5591d64712aa5d3b92ff1bc59caee5c77693d484
Lukas Holler <lukas.holler@live.de>
2022-05-25 20:31:42 +0200

    fixed docker>azure build

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -9,10 +9,10 @@ const { appId } = config;
 // Use `auth0Domain` in string interpolation below so that it doesn't
 // get replaced by the quickstart auto-packager
 const auth0Domain = domain;
-const iosOrAndroid = isPlatform("ios") || isPlatform("android");
+const isApp = isPlatform("capacitor");
 
 export const callbackUri = environment.production 
-  ? iosOrAndroid
+  ? isApp
     ? `${appId}://${auth0Domain}/capacitor/${appId}/callback`
     : "https://dhbw-experts.azurewebsites.net/callback"
   : "http://localhost:4200/callback";
